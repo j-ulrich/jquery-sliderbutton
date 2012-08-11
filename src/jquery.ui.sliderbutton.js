42b104f94b4cab0db8a2eb3452b1e85c09bb5d62
julrich <jochenulrich@t-online.de>
2012-08-11 03:08:20 +0200

    Code cleanup.
    * Removes many unnecessary .find() calls by saving the results internally.

--- src/jquery.ui.sliderbutton.js
+++ src/jquery.ui.sliderbutton.js
@@ -22,10 +22,6 @@
 		_create: function() {
 			var self = this;
 			
-			$.extend(self, {
-				activated: false
-			});
-			
 			self.element.empty();
 			self.element.addClass("ui-sliderbutton");
 			if (self.options.disabled === true) {
@@ -33,7 +29,7 @@
 			}
 			var text = $('<span></span>').addClass("ui-sliderbutton-text").text(self.options.text);
 			var slider = $('<div></div>').addClass("ui-sliderbutton-slider");
-			var handle = $('<div></div>').addClass("ui-slider-handle slide-button-handle");
+			var handle = $('<div></div>').addClass("ui-slider-handle");
 			slider.append(handle);
 			self.element.append(text);
 			self.element.append(slider);
@@ -42,6 +38,15 @@
 				self.element.addClass("ui-sliderbutton-left");
 				startValue = 100;
 			}
+			
+			$.extend(self, {
+				slider: slider,
+				handle: handle,
+				text: text,
+				activated: false // Prevents multiple activations for the same slide
+			});
+
+			
 			slider.slider({
 				animate: true,
 				value: startValue,
@@ -49,7 +54,7 @@
 				slide: function(event, ui) {
 					var allowed = self._trigger("slide", event, ui);
 					if (allowed !== false) {
-						self.element.find('.ui-sliderbutton-text').css("opacity",self.options.opacity((self.options.direction === "left"?(100-ui.value):ui.value)));
+						self.text.css("opacity",self.options.opacity((self.options.direction === "left"?(100-ui.value):ui.value)));
 						if ( (self.activated === false) && ((self.options.direction === "right" && ui.value >= (100-self.options.tolerance))
 								|| (self.options.direction === "left" && ui.value <= (0+self.options.tolerance))) ) {
 							self._trigger("activate", event, ui);
@@ -61,7 +66,7 @@
 				stop: function(event, ui) {
 					var allowed = self._trigger("stop", event, ui);
 					if (allowed !== false) {
-						self.reset("fast");
+						self._reset("fast");
 					}
 				},
 				start: function(event, ui) {
@@ -70,33 +75,33 @@
 			});
 		},
 		
-		reset: function(animationDuration) {
+		_reset: function(animationDuration) {
 			var self = this;
 			self.activated = false;
 			if (animationDuration === undefined || animationDuration === null) {
 				if (self.options.direction === "right") {
-					self.element.find(".ui-slider-handle").css("left", 0);
+					self.handle.css("left", 0);
 				}
 				else if (self.options.direction === "left") {
-					self.element.find(".ui-slider-handle").css("left", "100%");
+					self.handle.css("left", "100%");
 				}
-				self.element.find('.ui-sliderbutton-text').css("opacity",self.options.opacity(0));
+				self.text.css("opacity",self.options.opacity(0));
 			}
 			else {
 				if (self.options.direction === "right") {
-					self.element.find(".ui-slider-handle").animate({left: 0}, animationDuration);
+					self.handle.animate({left: 0}, animationDuration);
 				}
 				else if (self.options.direction === "left") {
-					self.element.find(".ui-slider-handle").animate({left: "100%"}, animationDuration);
+					self.handle.animate({left: "100%"}, animationDuration);
 				}
-				self.element.find('.ui-sliderbutton-text').animate({opacity: self.options.opacity(0)}, animationDuration);
+				self.text.animate({opacity: self.options.opacity(0)}, animationDuration);
 			}
 
 		},
 		
 		destroy: function() {
 			var self = this;
-			self.element.find('.ui-slider').slider("destroy");
+			self.slider.slider("destroy");
 			self.element.empty();
 			self.element.removeClass("ui-sliderbutton ui-sliderbutton-left");
 		},
@@ -107,17 +112,17 @@
 			
 			switch(option) {
 			case "text":
-				self.element.find('.ui-sliderbutton-text').text(value);
+				self.text.text(value);
 				break;
 			case "disabled":
 				if (value === true) {
-					self.element.find('.ui-slider').slider("disable");
+					self.slider.slider("disable").removeClass("ui-state-disabled");
 					self.element.addClass("ui-state-disabled");
 					self.element.attr("disabled", true);
-					self.element.find('.ui-sliderbutton-text').css("opacity","");
+					self.text.css("opacity","");
 				}
 				else if (value === false) {
-					self.element.find('.ui-slider').slider("enable");
+					self.slider.slider("enable");
 					self.element.attr("disabled", false);
 					self.element.removeClass("ui-state-disabled");
 				}
@@ -129,6 +134,7 @@
 				else if (value === "left") {
 					self.element.addClass("ui-sliderbutton-left");
 				}
+				self._reset();
 				break;
 			default:
 				break;
