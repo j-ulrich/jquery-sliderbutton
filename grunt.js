6c02f45b6c5aea01418e8b1f5014b2d85c52e6be
Jochen Ulrich <jochenulrich@t-online.de>
2014-06-09 13:26:12 +0200

    Updates contributors list.

--- grunt.js
+++ grunt.js
@@ -15,13 +15,13 @@ module.exports = function(grunt) {
 			'ui_banner': '/*! <%= pkg.title || pkg.name %> - jQuery UI Version - v<%= pkg.version %> - ' +
 				'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
 				'<%= pkg.homepage ? " * " + pkg.homepage + "\n" : "" %>' +
-				' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.contributors[0].name %>;' +
+				' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
 				' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %>\n'+
 				' */',
 			'mobile_banner': '/*! <%= pkg.title || pkg.name %> - jQuery Mobile Version - v<%= pkg.version %> - ' +
 				'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
 				'<%= pkg.homepage ? " * " + pkg.homepage + "\n" : "" %>' +
-				' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.contributors[0].name %>;' +
+				' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
 				' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %>\n'+
 				' */'
 		},
